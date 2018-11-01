ad05ad1c0ab9a257069366f18c487e04f5c49b5f
Megan Fago <meganlaynefago@gmail.com>
2018-11-01 00:45:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,9 +13,8 @@ end
 def not_safe?(speed)
   if speed = 79
 speed > 40? true: false
-elsif speed = 35
+else speed = 35
 speed < 40? true: false
-else speed = 50
   speed < 60 == speed > 40? true: false
 end
 	end
